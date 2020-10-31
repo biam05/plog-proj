a1c844a1267be42fccff01974ca037a198773220
Hugomguima <50011557+Hugomguima@users.noreply.github.com>
2020-10-31 11:28:36 +0000

    Update main.pl

--- src/main.pl
+++ src/main.pl
@@ -43,12 +43,12 @@ intermediate([
     [empty,  empty,    empty,    empty,    green,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty,    empty,    empty,    empty],
     [empty,  empty,    empty,    green,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty,    empty,    empty],
     [empty,  empty,    green,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty,    empty],
-    [empty,  green,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty],
-    [green,  position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple],
-    [empty,  empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    empty],
-    [purple, position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green],
-    [empty,  purple,   position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty],    
-    [empty,  empty,    purple,   position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty,    empty],
+    [empty,  green,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    purplePlayer, purple,   empty],
+    [green,  position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    greenPlayer, empty,    position, purple],
+    [empty,  empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    purplePlayer, empty,    position, empty,    empty],
+    [purple, position, empty,    position, empty,    position, empty,    purplePlayer, empty,    position, empty,    position, empty,    position, empty,    greenPlayer, empty,    greenPlayer, empty,    position, empty,    position, green],
+    [empty,  purple,   position, empty,    position, empty,    greenPlayer, empty,    position, empty,    position, empty,    position, empty,    position, empty,    purplePlayer, empty,    position, empty,    position, green,    empty],    
+    [empty,  empty,    purple,   purplePlayer, empty,    purplePlayer, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    purplePlayer, empty,    position, green,    empty,    empty],
     [empty,  empty,    empty,    purple,   position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty,    empty,    empty],  
     [empty,  empty,    empty,    empty,    purple,   position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty,    empty,    empty,    empty],
     [empty,  empty,    empty,    empty,    empty,    empty,    empty,    empty,    position, empty,    position, empty,    position, empty,    position, empty,    empty,    empty,    empty,    empty,    empty,    empty,    empty],
@@ -56,9 +56,28 @@ intermediate([
 ]).
 
 
+final([
+    [empty,  empty,    empty,          empty,        empty,       empty,        empty,    empty,    orange,   empty,    orange,   empty,    orange,   empty,    orange,   empty,    empty,    empty,    empty,    empty,    empty,    empty,    empty],
+    [empty,  empty,    empty,          empty,        empty,       empty,        empty,    empty,    position, empty,    position, empty,    position, empty,    position, empty,    empty,    empty,    empty,    empty,    empty,    empty,    empty],
+    [empty,  empty,    empty,          empty,        green,       position,     empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty,    empty,    empty,    empty],
+    [empty,  empty,    empty,          green,        position,    empty,        orangePlayer, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty,    empty,    empty],
+    [empty,  empty,    green,          position,     empty,       position,     empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty,    empty],
+    [empty,  green,    position,       empty,        greenPlayer, empty,        orangePlayer, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple,   empty],
+    [green,  position, empty,          position,     empty,       greenPlayer,  empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, purple],
+    [empty,  empty,    position,       empty,        greenPlayer, empty,        purplePlayer, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    empty],
+    [purple, position, empty,          position,     empty,       purplePlayer, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green],
+    [empty,  purple,   position,       empty,        greenPlayer, empty,        position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty],    
+    [empty,  empty,    purpleplayer,   purpleplayer, empty,       position,     empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty,    empty],
+    [empty,  empty,    empty,          purple,       position,    empty,        position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty,    empty,    empty],  
+    [empty,  empty,    empty,          empty,        purple,      position,     empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, empty,    position, green,    empty,    empty,    empty,    empty],
+    [empty,  empty,    empty,          empty,        empty,       empty,        empty,    empty,    position, empty,    position, empty,    position, empty,    position, empty,    empty,    empty,    empty,    empty,    empty,    empty,    empty],
+    [empty,  empty,    empty,          empty,        empty,       empty,        empty,    empty,    orange,   empty,    orange,   empty,    orange,   empty,    orange,   empty,    empty,    empty,    empty,    empty,    empty,    empty,    empty]
+]).
+
+
 % Board hard coded
-/*printBoard :-
-    write('        L L L L L        '),nl,
+/*initial :-
+    write('        O O O O O        '),nl,
     write('        x x x x x        '),nl,
     write('    Gx x x x x x x xP    '),nl,
     write('   Gx x x x x x x x xP   '),nl,
@@ -72,7 +91,25 @@ intermediate([
     write('   Px x x x x x x x xG   '),nl,
     write('    Px x x x x x x xG    '),nl,
     write('        x x x x x        '),nl,
-    write('        L L L L L        '),nl.
+    write('        O O O O O        '),nl.
+*/
+
+/*intermediate :-
+    write('        O O O O O        '),nl,
+    write('        x x x x x        '),nl,
+    write('    Gx x x x x x x xP    '),nl,
+    write('   Gx x x x x x x x xP   '),nl,
+    write('  Gx x x x x x x x x xP  '),nl,
+    write(' Gx x x x x x x x x x xP '),nl,
+    write('Gx x x x x x x x x x x xP'),nl,
+    write('  x x x x x x x x x x x  '),nl,
+    write('Px x x x x x x x x x x xG'),nl,
+    write(' Px x x x x x x x x x xG '),nl,
+    write('  Px x x x x x x x x xG  '),nl,
+    write('   Px x x x x x x x xG   '),nl,
+    write('    Px x x x x x x xG    '),nl,
+    write('        x x x x x        '),nl,
+    write('        O O O O O        '),nl.
 */
 
 % Funções para dar draw e cada elemento
@@ -91,14 +128,15 @@ display_game([Head|Tail],Player) :-
     nl,
     display_game(Tail,Player).
 
+display_game([],_) :-
+    write('Wrong Player given').
+
 display_game([],player1) :-
     write('Player1 turn').
 
 display_game([],player2) :-
     write('Player2 turn').
 
-display_game([],_) :-
-    write('Wrong Player given').
 
 printLine([Head|Tail]) :-
     symbol(Head, S),
