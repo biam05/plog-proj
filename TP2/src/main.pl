bd4f84f849467963b1f2b66255d9304ecc2a22f4
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-12-30 15:14:47 +0000

    testes com random rowvalues e colvalues

--- TP2/src/main.pl
+++ TP2/src/main.pl
@@ -98,3 +98,65 @@ exactly(X, [Y|L], N) :-
     X #= Y #<=> B,
     N #= M + B,
 exactly(X, L, M).
+
+selRandom(Var, Rest, BB0, BB1):- % seleciona valor de forma aleatória
+    fd_set(Var, Set), fdset_to_list(Set, List),
+    random_member(Value, List), % da library(random)
+    ( first_bound(BB0, BB1), Var #= Value ;
+    later_bound(BB0, BB1), Var #\= Value ).
+
+main_test(Length):-
+    % Declaração de variáveis
+    
+    length(RowValues,Length), % Valores na vertical (2,13,29,31)
+    length(ColValues,Length), % Valores na horizontal (3,11,23,41)
+
+    %RowValues = [2,13,29,31], % Valores na vertical (2,13,29,31)
+    %ColValues = [3,11,23,41], % Valores na horizontal (3,11,23,41)
+
+    % Gerar uma lista de listas dinâmica tendo em conta o parâmetro fornecido
+    generateListofLists(Length,ListOfLists),
+
+    % Gerar a matriz transposta para poder aceder às colunas
+    transpose(ListOfLists,Transpose),
+    
+    % Dar flatten à lista de modo a que seja possível atribuir um domínio
+    append(ListOfLists,Table),
+
+    % Restrições de domínio
+    TableValues is Length * 2,
+    TableMaxValue is TableValues * (TableValues - 1),
+
+    domain(Table,0,TableValues), % 8 porque é 2*número_de_rows(aka 4)
+    domain(RowValues,0,TableMaxValue),
+    domain(ColValues,0,TableMaxValue),
+
+    % Condições de jogo
+
+    % Os valores têm que ser todos distintios, quando diferentes de 0
+    all_distinct_except_0(Table),
+
+    % Cada linha ou coluna deve ter exatamente 2 valores diferentes de 0
+    Amount is Length - 2,
+
+    % Linhas
+    line_restriction(ListOfLists,Amount),
+    %Colunas
+    line_restriction(Transpose,Amount),
+
+    % Ensuring lines and columns multiply to correct value
+    multiplication_restriction(ListOfLists,RowValues),
+    multiplication_restriction(Transpose,ColValues),
+
+    % Cálculo de resultados
+    labeling([value(selRandom)],ColValues),
+    write('ColValues: '), write(ColValues), nl,
+    labeling([value(selRandom)],RowValues),
+    write('RowValues: '), write(RowValues), nl,
+    labeling([], Table),
+    %write('ColValues: '), write(ColValues), nl,
+    %write('RowValues: '), write(RowValues), nl,
+    %labeling([],Table),
+    %labeling([],ColValues),
+
+    display(ListOfLists, RowValues, ColValues, Length, TableValues).
