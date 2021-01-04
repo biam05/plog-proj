67c25996c5f1b799436133eba1316a5b6f338330
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2021-01-04 21:04:05 +0000

    documentação wrong products

--- TP2/src/wrongproducts.pl
+++ TP2/src/wrongproducts.pl
@@ -1,8 +1,26 @@
+% ------------------------------------------------------------------------------------
+% ----------------------------------- BIBLIOTECAS ------------------------------------
+% ------------------------------------------------------------------------------------
+
+:- use_module(library(lists)).
+:- use_module(library(random)). % POde ser necessário retirar no fim se não se usar
+:- use_module(library(clpfd)). % Permite utilizar o módulo de restrições
+
+% ------------------------------------------------------------------------------------
+% ----------------------------------- FICHEIROS  -------------------------------------
+% ------------------------------------------------------------------------------------
+
 :- ensure_loaded(display).
-:- ensure_loaded(main).
+:- ensure_loaded(problem).
 :- ensure_loaded(inputs).
 
+% ------------------------------------------------------------------------------------
+% ------------------------------- INÍCIO DO PROGRAMA  --------------------------------
+% ------------------------------------------------------------------------------------
+
+% main.
+% predicado que se deve correr para iniciar o pgroama, apresenta o menu de opções
+% e recebe o input da opção escolhida pelo utilizador
 main:-
     menuDisplay,
     readOption.
-    %runner(4).
